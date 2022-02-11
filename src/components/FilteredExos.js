a1cd5ce393b5e2439731820b17f759e707831d02
brazillierjo <j.brazillier@gmail.com>
2022-02-11 20:40:40 +0100

    bdd update

--- src/components/FilteredExos.js
+++ src/components/FilteredExos.js
@@ -14,7 +14,7 @@ export default function FilteredExos(props) {
     }, [props.exercices])
 
     return <div>
-        <div className='flex justify-around bg-gray-200 px-2 py-1 mx-8 rounded-md outline-none'>
+        <div className='flex flex-wrap justify-around bg-gray-200 px-2 py-1 mx-8 rounded-md outline-none'>
             {group?.map(group => {
                 return <button className={selectedMuscle === group ? 'bg-teal-500 shadow px-8 py-2.5 text-sm leading-5 font-medium text-white rounded-md outline-none focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60' : 'px-8 py-2 rounded-md'} key={group} onClick={() => setSelectedMuscle(group)}>{group}</button>
             })}
