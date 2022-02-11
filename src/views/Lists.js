71d64462f8250236c09f838e778ba8e98a9d3c1c
brazillierjo <j.brazillier@gmail.com>
2022-02-11 13:15:55 +0100

    filter page

--- src/views/Lists.js
+++ src/views/Lists.js
@@ -9,40 +9,45 @@ function classNames(...classes) {
 }
 
 export default function Lists() {
-    const [exercices, setExercices] = useState([]);
+    const [exercices, setExercices] = useState([])
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
+        setLoading(true)
         setExercices(workoutexercices);
+        setLoading(false)
     }, []);
 
     return (
         <div>
-            <Tab.Group>
-                <Tab.List className="flex p-1 space-x-1 bg-blue-900/20 rounded-xl my-4 mx-2">
-                    <Tab className={({ selected }) =>
-                        classNames(
-                            'w-full py-2.5 text-sm leading-5 font-medium text-white rounded-lg',
-                            'focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60',
-                            selected
-                                ? 'bg-teal-500 shadow'
-                                : 'text-black hover:bg-white/[0.12]'
-                        )
-                    }>Tous les exercices</Tab>
-                    <Tab className={({ selected }) =>
-                        classNames(
-                            'w-full py-2.5 text-sm leading-5 font-medium text-white rounded-lg',
-                            'focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60',
-                            selected
-                                ? 'bg-teal-500 shadow'
-                                : 'text-black hover:bg-white/[0.12]'
-                        )
-                    }>Exercices par muscles</Tab>
-                </Tab.List>
-                <Tab.Panels>
-                    <Tab.Panel><AllExos exercices={exercices} /></Tab.Panel>
-                    <Tab.Panel><FilteredExos exercices={exercices} /></Tab.Panel>
-                </Tab.Panels>
-            </Tab.Group>
+            {loading ?
+                <div> Loading... </div> :
+                <Tab.Group>
+                    <Tab.List className="flex p-1 space-x-1 bg-blue-900/20 rounded-xl my-4 mx-2">
+                        <Tab className={({ selected }) =>
+                            classNames(
+                                'w-full py-2.5 text-sm leading-5 font-medium text-white rounded-lg',
+                                'focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60',
+                                selected
+                                    ? 'bg-teal-500 shadow'
+                                    : 'text-black hover:bg-white/[0.12]'
+                            )
+                        }>Tous les exercices</Tab>
+                        <Tab className={({ selected }) =>
+                            classNames(
+                                'w-full py-2.5 text-sm leading-5 font-medium text-white rounded-lg',
+                                'focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60',
+                                selected
+                                    ? 'bg-teal-500 shadow'
+                                    : 'text-black hover:bg-white/[0.12]'
+                            )
+                        }>Exercices par muscles</Tab>
+                    </Tab.List>
+                    <Tab.Panels>
+                        <Tab.Panel><AllExos exercices={exercices} /></Tab.Panel>
+                        <Tab.Panel><FilteredExos exercices={exercices} /></Tab.Panel>
+                    </Tab.Panels>
+                </Tab.Group>}
         </div>
     )
 }
